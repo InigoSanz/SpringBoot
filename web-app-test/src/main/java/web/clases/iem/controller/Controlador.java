f258e9fd7cf696c9e56102f8709b2729c45e0bee
InigoSanz <i.sanzdelg@gmail.com>
2025-03-04 16:00:00 +0100

    Solución import Request
    Había un import de una librería erronea

--- web-app-test/src/main/java/web/clases/iem/controller/Controlador.java
+++ web-app-test/src/main/java/web/clases/iem/controller/Controlador.java
@@ -14,11 +14,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import web.clases.iem.dto.Coche;
 
 @RestController
