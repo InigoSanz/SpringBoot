929ff4e98306da36b7f916c41db0ceacfd25cb5f
InigoSanz <i.sanzdelg@gmail.com>
2025-03-09 18:09:31 +0100

    Ejercicio SpringBoot REST
    Servicio para comprobar las ITVs de los vehículos hecho.
    
    Falta programar cuando se ejecuta y mostrar el vehiculo que si o que no esta disponible mediante los logs.

--- iem-ejercicio-uber/src/main/java/com/iem/rest/service/ComprobarItvService.java
+++ iem-ejercicio-uber/src/main/java/com/iem/rest/service/ComprobarItvService.java
@@ -1,6 +1,8 @@
 package com.iem.rest.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,26 +12,38 @@ import org.springframework.stereotype.Service;
 import com.iem.rest.controller.UberController;
 import com.iem.rest.dto.Vehiculo;
 
+/**
+ * Clase para los servicios.
+ */
 @Service
 public class ComprobarItvService {
 	
 	private static final Logger log = LoggerFactory.getLogger(UberController.class);
 	
-	Vehiculo vehiculo = new Vehiculo();
+	private List<Vehiculo> vehiculos = new ArrayList<>();
 	
-	public ComprobarItvService() {
-		
+//	Vehiculo vehiculo = new Vehiculo();
+	
+	public ComprobarItvService(List<Vehiculo> vehiculos) {
+		this.vehiculos = vehiculos;
 	}
 	
+	/**
+	 * Tarea programada para comprobar las ITVs de los vehículos.
+	 */
 	@Scheduled
 	public void comprobarItv() {
 		log.info("Comprobando estado de la ITV...");
 		
 		LocalDateTime ahora = LocalDateTime.now(); // De momento vamos a utilizar la fecha actual como referencia, pero habría que utilizar la fecha de la última ITV que ha pasado
-		LocalDateTime revision = vehiculo.getFechaSiguienteItv(); // Fijamos la fecha a un año vista
+//		LocalDateTime revision = vehiculo.getFechaSiguienteItv(); // Fijamos la fecha a un año vista
 		
 		for (Vehiculo vehiculo : vehiculos) {
-			
+			LocalDateTime revision = vehiculo.getFechaSiguienteItv(); // Cogemos la fecha de la siguiente ITV de cada vehículo
+			if (revision.isBefore(ahora)) {
+				vehiculo.setDisponible(false);
+				log.info("El vehículo no esta disponible para el viaje"); // Lo suyo sería mostrar que vehiculo es el que no esta disponible y cual si
+			}
 		}
 	}
 }
