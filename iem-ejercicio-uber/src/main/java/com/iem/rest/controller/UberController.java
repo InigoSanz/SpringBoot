f7c9e10c61e2314af86e2e89cf34d0c8cfa0d7c8
InigoSanz <i.sanzdelg@gmail.com>
2025-03-09 12:05:01 +0100

    Ejercicio SpringBoot REST
    Hechos los métodos AltaVehiculo y ObtenerVehiculos.

--- iem-ejercicio-uber/src/main/java/com/iem/rest/controller/UberController.java
+++ iem-ejercicio-uber/src/main/java/com/iem/rest/controller/UberController.java
@@ -9,8 +9,10 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -19,16 +21,22 @@ import com.iem.rest.dto.Vehiculo;
 /**
  * Controlador REST de la aplicación.
  */
-@RestController("/uber")
+@RestController
+@RequestMapping("/uber")
 public class UberController {
 
 	private static final Logger log = LoggerFactory.getLogger(UberController.class);
 
 	List<Vehiculo> vehiculos = new ArrayList<>();
 	LocalDateTime ahora = LocalDateTime.now();
-
+	
+	/**
+	 * Dar de alta Vehículos con: el número de bastidor, la matrícula y fecha de siguiente ITV.
+	 * @param vehiculo
+	 * @return
+	 */
 	@PostMapping("/vehiculo")
-	public ResponseEntity<Void> darAltaVehiculo(@RequestBody Vehiculo vehiculo) {
+	public ResponseEntity<Void> darAltaVehiculo(@RequestBody Vehiculo vehiculo) { // Al poner request body, Spring necesita que le enviemos algun dato por json, si no falla
 		log.debug("Alta vehículo");
 
 		String numeroBastidor = UUID.randomUUID().toString();
@@ -42,6 +50,21 @@ public class UberController {
 		URI uri = crearUri(numeroBastidor);
 		
 		return ResponseEntity.created(uri).build();
+	}
+	
+	/**
+	 * Consultar Vehículos y que nos devuelva: el número de bastidor, la matrícula, los kilómetros recorridos totales y fecha de siguiente ITV.
+	 * @return vehiculos
+	 */
+	@GetMapping("/vehiculos")
+	public ResponseEntity<List<Vehiculo>> getVehiculos() {
+		log.debug("Obtener vehículos");
+		
+		if (!vehiculos.isEmpty()) {
+			return ResponseEntity.ok(vehiculos);
+		}
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 	/**
