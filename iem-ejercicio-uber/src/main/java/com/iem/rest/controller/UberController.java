cad1d70441c285f2ff261383d9cb17b6f91a96a1
InigoSanz <i.sanzdelg@gmail.com>
2025-03-09 12:40:11 +0100

    Ejercicio SpringBoot REST
    En los endpoints para obtener los conductores y viajes de un vehículo nos da error 500 ya que esas listas son null.
    
    Vamos a solucionarlo.

--- iem-ejercicio-uber/src/main/java/com/iem/rest/controller/UberController.java
+++ iem-ejercicio-uber/src/main/java/com/iem/rest/controller/UberController.java
@@ -10,13 +10,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.iem.rest.dto.Conductor;
 import com.iem.rest.dto.Vehiculo;
+import com.iem.rest.dto.Viaje;
 
 /**
  * Controlador REST de la aplicación.
@@ -28,6 +31,8 @@ public class UberController {
 	private static final Logger log = LoggerFactory.getLogger(UberController.class);
 
 	List<Vehiculo> vehiculos = new ArrayList<>();
+	List<Conductor> conductores = new ArrayList<>();
+	List<Viaje> viajes = new ArrayList<>();
 	LocalDateTime ahora = LocalDateTime.now();
 	
 	/**
@@ -65,6 +70,68 @@ public class UberController {
 		}
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	/**
+	 * Obtiene la información de un vehículo según su número de bastidor.
+	 * @param nBastidor
+	 * @return vehiculo
+	 */
+	@GetMapping("/vehiculos/{nBastidor}")
+	public ResponseEntity<Vehiculo> getVehiculo(@PathVariable("nBastidor") String nBastidor) {
+		log.debug("Obtener un vehículo");
+		
+		for (Vehiculo vehiculo : vehiculos) {
+			if (vehiculo.getnBastidor().equals(nBastidor)) {
+				return ResponseEntity.ok(vehiculo);
+			}
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	/**
+	 * Obntiene los conductores de un vehículo según su número de bastidor.
+	 * @param nBastidor
+	 * @return conductores de un vehículo
+	 */
+	@GetMapping("/vehiculos/{nBastidor}/conductores")
+	public ResponseEntity<List<Conductor>> getConductoresVehiculo(@PathVariable("nBastidor") String nBastidor) {
+		log.debug("Obtener conductores de vehículos");
+		
+		for(Vehiculo vehiculo : vehiculos) {
+			if (vehiculo.getnBastidor().equals(nBastidor)) {
+				if (!vehiculo.getConductores().isEmpty()) {
+					return ResponseEntity.ok(vehiculo.getConductores());
+				} else {
+					return ResponseEntity.noContent().build();
+				}
+			}
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	/**
+	 * Obntiene los viajes de un vehículo según su número de bastidor.
+	 * @param nBastidor
+	 * @return viajes de un vehículo
+	 */
+	@GetMapping("/vehiculos/{nBastidor}/viajes")
+	public ResponseEntity<List<Viaje>> getViajesVehiculo(@PathVariable("nBastidor") String nBastidor) {
+		log.debug("Obtener viajes de vehículos");
+		
+		for(Vehiculo vehiculo : vehiculos) {
+			if (vehiculo.getnBastidor().equals(nBastidor)) {
+				if (!vehiculo.getViajes().isEmpty()) {
+					return ResponseEntity.ok(vehiculo.getViajes());
+				} else {
+					return ResponseEntity.noContent().build();
+				}
+			}
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 	/**
