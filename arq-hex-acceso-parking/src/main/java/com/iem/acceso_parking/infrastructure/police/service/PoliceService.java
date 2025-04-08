240495eb955c0ef92479a346a8b982753ebb2f61
InigoSanz <i.sanzdelg@gmail.com>
2025-04-08 19:07:56 +0200

    Ejercicio Arquitectura Hexagonal
    Ejercicio terminado, estructura completa.

--- arq-hex-acceso-parking/src/main/java/com/iem/acceso_parking/infrastructure/police/service/PoliceService.java
+++ arq-hex-acceso-parking/src/main/java/com/iem/acceso_parking/infrastructure/police/service/PoliceService.java
@@ -1,10 +1,20 @@
 package com.iem.acceso_parking.infrastructure.police.service;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import com.iem.acceso_parking.application.port.PoliceOutputPort;
 
 @Component
-public class PoliceService implements PoliceOutputPort{
+public class PoliceService implements PoliceOutputPort {
 
+	private RestTemplate restTemplate = new RestTemplate();
+
+	@Override
+	public void call() {
+		
+		restTemplate.exchange("http://host_police/call", HttpMethod.POST, HttpEntity.EMPTY, String.class);
+	}
 }
